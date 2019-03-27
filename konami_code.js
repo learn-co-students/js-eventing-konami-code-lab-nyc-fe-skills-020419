b812c66253b0b3e16024e5c335831eafd24e9aef
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-03-27 22:45:39 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let tracker = 0
 
-}
+function init(){
+  function onKeyDown(event){
+   let key = parseInt(event.which)
+  
+    if (key === code[tracker]){
+      tracker++
+   } else{
+   tracker = 0;
+   }
+   
+   if (tracker === code.length){
+      alert('Booyakasha!')
+      tracker = 0;
+
+   } 
+  }
+
+  document.addEventListener('keydown', onKeyDown)
+  }
+
+init()
+
