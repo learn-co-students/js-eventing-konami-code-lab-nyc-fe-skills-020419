37359a25bbf0688a980d73e14d9078ad3c1bd3b2
Hannah Coffman <hcoffman7@gmail.com>
2019-03-27 22:45:20 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let tracker = 0
 
+function init() {}
+  
+function onKeyDown(event) {
+  if (event.keyCode === code[tracker]) {
+    tracker++ 
+  } else {
+  tracker = 0
+  }
+ if (tracker === 10) {
+ alert('Nice job!')
 }
+}
+
+ // event //callback (trigger something)
+document.addEventListener('keydown', onKeyDown)
+                           
+init()
+
+//  let key = parseInt(event.which)
+//  console.log(event.which);
+//if (key === code[tracker])
